e365dd382c90870be07aff106e83b634d0d041cc
DESKTOP-NJM1EHE\vlad <vlad@lesnoy.name>
2017-08-22 23:38:56 +0300

    uncommented showToastr methods

--- src/android/CordovaDeviceAdminReceiver.java
+++ src/android/CordovaDeviceAdminReceiver.java
@@ -7,14 +7,14 @@ import android.widget.Toast;
 
 public class CordovaDeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
 
-//    void showToast(Context context, String msg) {
-//        String status = context.getString(R.string.admin_receiver_status, msg);
-//        Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
-//    }
+    void showToast(Context context, String msg) {
+        String status = context.getString(R.string.admin_receiver_status, msg);
+        Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public void onEnabled(Context context, Intent intent) {
-//        showToast(context, context.getString(R.string.admin_receiver_status_enabled));
+        showToast(context, context.getString(R.string.admin_receiver_status_enabled));
     }
 
     @Override
@@ -25,7 +25,7 @@ public class CordovaDeviceAdminReceiver extends android.app.admin.DeviceAdminRec
 
     @Override
     public void onDisabled(Context context, Intent intent) {
-//        showToast(context, context.getString(R.string.admin_receiver_status_disabled));
+        showToast(context, context.getString(R.string.admin_receiver_status_disabled));
     }
 
     @Override
